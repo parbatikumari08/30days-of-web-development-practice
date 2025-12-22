de07bbd0065d246d0df36c19c3360926aecafed0
Parbati Kumari <shawparo08@gmail.com>
2025-12-22 16:20:40 +0530

    feat(day24): add reusable fetch hook

---
+++ day24/useFetch.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(setData)
+      .finally(() => setLoading(false));
+  }, [url]);
+
+  return { data, loading };
+}
